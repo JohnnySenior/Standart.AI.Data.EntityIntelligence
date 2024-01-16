3e3e080a1c00cab4a422b45b5a9fda96bac12043
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-16 22:21:39 +0900

    INFRA: Initialize EI.Tests.Integration

---
+++ EntityIntelligence.Tests.Integration/DeleteMe.cs
@@ -0,0 +1,12 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ---------------------------------------------------------------------------------
+
+namespace EntityIntelligence.Tests.Integration
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
