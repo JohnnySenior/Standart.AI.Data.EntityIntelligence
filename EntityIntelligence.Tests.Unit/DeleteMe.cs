9f87418834bfbe61712093762620f0ebf9ebe3e8
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-16 21:55:21 +0900

    INFRA: Initialize EI.Core.Tests.Unit

---
+++ EntityIntelligence.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace EntityIntelligence.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
