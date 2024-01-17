c594c5db8b62b1cf820b0469f651077645ff765f
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-17 17:07:41 +0900

    INFRA: Initialize EI.Tests.Acceptence
    Closes #4

--- EntityIntelligence.Tests.Acceptrnce/DeleteMe.cs
+++ EntityIntelligence.Tests.Acceptrnce/DeleteMe.cs
@@ -1,10 +1,10 @@
 // ----------------------------------------------------------------------------------
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
-// ---------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
 
 using Xunit;
 
-namespace EntityIntelligence.Tests.Integration
+namespace EntityIntelligence.Tests.Acceptrnce
 {
     public class DeleteMe
     {
